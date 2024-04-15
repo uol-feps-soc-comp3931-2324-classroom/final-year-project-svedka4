5111d09a5b764837850a04291584f4f998a6c8aa
Gabija Svedkauskaite <gabija.svedkauskaite@coxautoinc.com>
2024-04-15 19:07:57 +0100

    creating database for experiment

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from app import app, db
-
-def create_database():
-    with app.app_context():
-        db.create_all()
+from app import app
 
 if __name__ == '__main__':
-    create_database()
     app.run(debug=True)
