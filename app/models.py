b2acefb98f98b70ae3abac6ec4dc1f445ea26116
Gabija Svedkauskaite <gabija.svedkauskaite@coxautoinc.com>
2024-04-15 19:08:18 +0100

    create tables

--- app/models.py
+++ app/models.py
@@ -1,15 +1,31 @@
 from app import db
 import datetime
+from flask import session
 
 class Session(db.Model):
+    __tablename__ = 'session'
+
     user_session_id = db.Column(db.String, primary_key=True)
     liked_genres = db.Column(db.String)
     mood_chosen = db.Column(db.String)
     mood_shift = db.Column(db.String)
     discovered_genres = db.Column(db.String)
 
+    @staticmethod
+    def create_session(user_session_id, liked_genres, mood_chosen, mood_shift, discovered_genres):
+        session_instance = Session(
+            user_session_id=user_session_id,
+            liked_genres=liked_genres,
+            mood_chosen=mood_chosen,
+            mood_shift=mood_shift,
+            discovered_genres=discovered_genres
+        )
+        db.session.add(session_instance)
+        db.session.commit()
 
 class Ratings(db.Model):
+    __tablename__ = 'ratings'
+
     current_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     song_id = db.Column(db.String, primary_key=True)
     mood_shift = db.Column(db.String)
@@ -17,3 +33,15 @@ class Ratings(db.Model):
     user_rating = db.Column(db.String)
     user_session_id = db.Column(db.String, db.ForeignKey('session'), primary_key=True)
     session = db.relationship('Session', backref='ratings')
+
+    @staticmethod
+    def create_rating(song_id, mood_shift, discovered_genres, user_rating, user_session_id):
+        rating_instance = Ratings(
+            song_id=song_id,
+            mood_shift=mood_shift,
+            discovered_genres=discovered_genres,
+            user_rating=user_rating,
+            user_session_id=user_session_id
+        )
+        db.session.add(rating_instance)
+        db.session.commit()
