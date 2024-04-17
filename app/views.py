bd15c845a9f75e3253525bb75b6535b3f22dad1f
Gabija Svedkauskaite <gabija.svedkauskaite@coxautoinc.com>
2024-04-17 12:20:50 +0100

    committing data to database

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-from app import app, recommender, ratings, mood_calc
+from app import app, recommender, ratings, mood_calc, models
 import os, uuid
 
 valid_genres = ['Rock','Folk', 'Blues', 'Pop', 'Country', 'Hip-hop', 'Jazz', 'SoulRB', 'Classical', 'Instrumental', 'Electronic', 'Experimental', 'International', 'Spoken']
@@ -13,13 +13,13 @@ def index():
 def genre():
     title = 'Music Player | Preferences'
 
-    session['user_session_id'] = str(uuid.uuid4())
-    print("Session ID: ", session['user_session_id'])
+    session['user_session_id'] = str(uuid.uuid4()) # user_session_id for Session model
     
     if request.method == 'POST':
         selected_genres = request.form.getlist('genre')
-
-        #Genre
+        session['selected_genres'] = selected_genres # liked_genres for Session model
+        
+        # Genre
         user_selected_weights = []
 
         # Get the selected genres from the user
@@ -51,7 +51,7 @@ def mood():
         # Calculate the users mood
         users_mood = mood_calc.mood_calc()
         session['users_mood'] = users_mood
-        session['ratings_impact_mood'] = users_mood
+        session['ratings_impact_mood'] = users_mood # initially calculated user mood
 
         return redirect(url_for('player', genre=session['selected_genres'], mood=selected_moods))
     
@@ -77,4 +77,20 @@ def submit_ratings():
 
     session['rating'] = rating
     ratings.user_ratings(valid_genres)
+
+
+
+    # Store in the database
+    user_session_id = session['user_session_id']
+    song_id = session['curr_song_info']['song_id']
+    mood_shift_after_rating = session['users_mood_after_each_rating']
+    user_rating = session['rating']
+    liked_genres = session['selected_genres']
+    mood_chosen = session['users_mood']
+    final_mood_shift = session['final_users_mood']
+    # discovered_genres = session['']
+
+    models.Session.create_session(user_session_id, liked_genres, mood_chosen, final_mood_shift, discovered_genres)
+    models.Ratings.create_rating(song_id, mood_shift_after_rating, discovered_genres, user_rating, user_session_id)
+    
     return redirect(url_for('player'))
